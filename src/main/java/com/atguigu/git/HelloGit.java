2578701ebb8b15992bf2b82dd484a147242f2724
Vtop-Xing <vtop_fadeaway.xing@foxmail.com>
2021-09-11 11:05:34 +0800

    Hello Git3

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -10,5 +10,6 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello World2");
+        System.out.println("Hello World2");
     }
 }
