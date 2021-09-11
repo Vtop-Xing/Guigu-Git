5f0159c8eee203857c9ca1903d13a8d6eb20123d
Vtop-Xing <vtop_fadeaway.xing@foxmail.com>
2021-09-11 10:37:53 +0800

    Hello Git

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @User Vtop-FadeAway
+ * @Date 2021/9/11
+ * @Time 10:16
+ * ❤ ¬_¬ 💘 ^_+ 💕
+ * You Had Me At Hello
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
